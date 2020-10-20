dbecf23ec7ad3a27f4a5fe41919b6c77387da4f1
Tim Hintz <tjhintz@gmail.com>
2020-10-20 08:33:40 -0400

    added 2 more key functions

--- src.py
+++ src.py
@@ -17,3 +17,14 @@ def get_interactions(df, choose=2):
     columns = itertools.permutations(df.columns,choose)
     interaction = {' X '.join(x): df[x[0]]*df[x[0]] for x in columns}
     return pd.DataFrame.from_dict(interaction)
+
+def multicolinear_features(data):
+    """
+    Given a data frame of features, this function returns a list of features that are colinear
+    """
+    df=data.corr().abs().stack().reset_index().sort_values(0, ascending=False)
+    df['pairs'] = list(zip(df.level_0, df.level_1))
+    df.set_index(['pairs'], inplace = True)
+    df.drop(columns=['level_1', 'level_0'], inplace = True)
+    df.columns = ['cc']
+    return df[(df.cc>.75) & (df.cc <1)]
